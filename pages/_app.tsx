7ed17c3c6c4cd530ab643fcf2875dfe367034a0c
Jarrod Watts <jarrodwatts16@gmail.com>
2022-05-30 11:49:04 +1000

    Add more info in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/guide/ThirdwebGuideFooter";
 import "./styles/globals.css";
 
 // This is the chainId your dApp will work on.
@@ -9,7 +11,17 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>thirdweb Signature Based Minting</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="thirdweb Example Repository to Showcase signature based minting on an NFT Collection contract"
+        />
+        <meta name="keywords" content="thirdweb signature based minting" />
+      </Head>
       <Component {...pageProps} />
+      <ThirdwebGuideFooter />
     </ThirdwebProvider>
   );
 }
