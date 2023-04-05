9c8483352f25387d2ec8f0aa5027605b9af19651
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-04-05 09:19:50 +0530

    feat: update template (#4)
    * feat: update template
    
    * remove unused imports
    
    * chore: update readme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import Head from "next/head";
 import ThirdwebGuideFooter from "../components/guide/ThirdwebGuideFooter";
 import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
+// This is the chain your dApp will work on.
+const activeChain = "goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider activeChain={activeChain}>
       <Head>
         <title>thirdweb Signature Based Minting</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
